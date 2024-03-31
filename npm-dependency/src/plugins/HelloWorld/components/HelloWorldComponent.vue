92b8b8470ab0e15cd499f5952b5eff29250633bd
John Hill <john.c.hill@nasa.gov>
2024-03-31 09:18:58 -0700

    first pass at updating to work with ESM and split into 3 project types

---
+++ npm-dependency/src/plugins/HelloWorld/components/HelloWorldComponent.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+    {{ domainObject.string }} For Hello World
+</div>
+</template>
+
+<script>
+import { inject } from 'vue';
+
+export default {
+    setup() {
+        // Use inject to access global properties
+        const openmct = inject('openmct');
+        const domainObject = inject('domainObject');
+
+        // Return the properties you want to use in your template
+        return { openmct, domainObject };
+    }
+};
+</script>
